c56d59ba9c6bc97027f4e1974bd00219d894614f
João Barioni <joao.barioni.2002@gmail.com>
2023-02-09 22:25:44 -0300

    stage1

--- apirequest.js
+++ apirequest.js
@@ -3,7 +3,7 @@ window.onload = function() {
   const dataContainer = document.getElementById('data-container');
 
   fetchDataButton.addEventListener('click', () => {
-    fetch('https://5uexc8ukrd.execute-api.us-east-1.amazonaws.com/default')
+    fetch('https://5uexc8ukrd.execute-api.us-east-1.amazonaws.com/Stage1')
       .then(response => response.json())
       .then(data => {
         dataContainer.innerHTML = JSON.stringify(data);
