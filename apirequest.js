b66250c72c19c35277317147eae69579f500d577
João Barioni <joao.barioni.2002@gmail.com>
2023-02-09 21:44:32 -0300

    final 1

--- apirequest.js
+++ apirequest.js
@@ -1,13 +1,15 @@
-const fetchDataButton = document.getElementById('fetch-data-button');
-const dataContainer = document.getElementById('data-container');
+window.onload = function() {
+  const fetchDataButton = document.getElementById('fetch-data-button');
+  const dataContainer = document.getElementById('data-container');
 
-fetchDataButton.addEventListener('click', () => {
-  fetch('https://5uexc8ukrd.execute-api.us-east-1.amazonaws.com/default')
-    .then(response => response.json())
-    .then(data => {
-      dataContainer.innerHTML = JSON.stringify(data);
-    })
-    .catch(error => {
-      dataContainer.innerHTML = 'An error occurred: ' + error;
-    });
-});
+  fetchDataButton.addEventListener('click', () => {
+    fetch('https://5uexc8ukrd.execute-api.us-east-1.amazonaws.com/default')
+      .then(response => response.json())
+      .then(data => {
+        dataContainer.innerHTML = JSON.stringify(data);
+      })
+      .catch(error => {
+        dataContainer.innerHTML = 'An error occurred: ' + error;
+      });
+  });
+};
