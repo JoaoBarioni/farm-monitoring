2ebda0fce4d4ebc7477857e5d3a8aa9ed8e29516
João Barioni <joao.barioni.2002@gmail.com>
2023-02-09 21:30:32 -0300

    add-url-api

--- apirequest.js
+++ apirequest.js
@@ -2,7 +2,7 @@ const fetchDataButton = document.getElementById('fetch-data-button');
 const dataContainer = document.getElementById('data-container');
 
 fetchDataButton.addEventListener('click', () => {
-  fetch('https://api.example.com/data')
+  fetch('https://5uexc8ukrd.execute-api.us-east-1.amazonaws.com/default')
     .then(response => response.json())
     .then(data => {
       dataContainer.innerHTML = JSON.stringify(data);
